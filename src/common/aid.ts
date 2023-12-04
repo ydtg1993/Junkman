9ce2e8e18c32424dd4f99b7df5496a19f02530b6
Junkman <pierkadan1993@gmail.com>
2023-12-04 14:56:04 +0800

    1

--- src/common/aid.ts
+++ src/common/aid.ts
@@ -3,7 +3,7 @@ export class Aid {
         if (remove) {
             dom.innerHTML = '';
             if (dom.parentNode instanceof HTMLElement) {
-                dom.parentNode.style.position =  dom.getAttribute('stash-position');
+                dom.parentNode.style.position = dom.getAttribute('stash-position') ?? '';
             }
             return;
         }
@@ -19,5 +19,40 @@ export class Aid {
             <div><div></div></div>
             <div><div></div></div>
         </div></div>`);
+    }
+
+    static contextmenu(doms: HTMLElement[],
+                       options: { title: string, func: any }[],
+                       width:string = "70px") {
+        doms.forEach((dom) => {
+            dom.addEventListener("contextmenu", (e: MouseEvent) => {
+                e.preventDefault();
+                if (e.target instanceof HTMLElement) e.target.click();
+
+                let ul = document.createElement('ul');
+                ul.className = "jk-contextmenu";
+                options.forEach((option) => {
+                    let li = document.createElement('li');
+                    li.insertAdjacentHTML('afterbegin', option.title);
+                    li.style.width = width;
+                    if (typeof option.func === 'function') {
+                        li.addEventListener('click', () => {
+                            ul.remove();
+                            option.func();
+                        });
+                        ul.append(li);
+                    }
+                });
+                ul.style.top = `${e.pageY - 3}px`;
+                ul.style.left = `${e.pageX - 3}px`;
+                ul.addEventListener("contextmenu", (e) => {
+                    e.preventDefault();
+                });
+                ul.addEventListener('mouseleave', () => {
+                    ul.remove();
+                });
+                document.getElementsByTagName("BODY")[0].appendChild(ul);
+            });
+        });
     }
 }
